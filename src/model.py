25afb4627cac8cf7fe499eec7094a2818702b469
Srijan Srivastava <QCoreNest@gmail.com>
2026-01-18 12:48:41 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -21,9 +21,7 @@ def apply_rotary_emb(x, cos, sin):
     x1, x2 = x[..., :d], x[..., d:] # split up last time into two halves
     y1 = x1 * cos + x2 * sin # rotate pairs of dims
     y2 = x1 * (-sin) + x2 * cos
-    out = torch.cat([y1, y2], 3) # re-assemble
-    out = out.to(x.dtype) # ensure input/output dtypes match
-    return out
+    return torch.cat([y1, y2], 3)
 
 class CastedLinear(nn.Module):
     def __init__(self, in_features, out_features):
@@ -31,19 +29,19 @@ class CastedLinear(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
 
-        self.w = nn.Parameter(torch.empty(out_features, in_features))
+        self.weight = nn.Parameter(torch.empty(out_features, in_features))
 
         # init params
         self.reset_parameters()
 
+    # transformer blocks: uniform init with bound = sqrt(3) * std (same standard deviation as normal)
     def reset_parameters(self):
-        std = 0.5 * (self.in_features ** -0.5) # 0.5 is a bit better than the default 1/sqrt(3)
-        bound = (3 ** 0.5) * std
+        s = 3**0.5 * self.in_features**-0.5 # sqrt(3) multiplier makes sure Uniform achieves the same std as Normal
         with torch.no_grad():
-            self.w.uniform_(-bound, bound)
+            self.weight.uniform_(-s, s)
 
     def forward(self, x):
-        return F.linear(x, self.w)
+        return F.linear(x, self.weight)
 
 class AttentionOnDetail(nn.Module):
     def __init__(self, config: Config):
@@ -57,7 +55,7 @@ class AttentionOnDetail(nn.Module):
         self.out = CastedLinear(config.n_embd, config.n_embd)
 
         # zero out c_proj weights in all blocks
-        torch.nn.init.zeros_(self.out.w)
+        torch.nn.init.zeros_(self.out.weight)
 
     def forward(self, x, cos_sin):
         # batch size, sequence length, embedding dimensionality (n_embd)
@@ -65,14 +63,15 @@ class AttentionOnDetail(nn.Module):
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k, v = self.qkv(norm(x)).view(B, T, 3*self.n_head, self.d_head).chunk(3, dim=2) # (B, T, nh, hs)
+
         # apply rotary embeddings to queries and keys to get relative positional encoding
         cos, sin = cos_sin
         q, k = apply_rotary_emb(q, cos, sin), apply_rotary_emb(k, cos, sin) # QK rotary embedding
         q, k = norm(q), norm(k) # QK norm
-        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2) # make head be batch dim, i.e. (B, T, nh, hs) -> (B, nh, T, hs)
 
-        # https://arxiv.org/pdf/2105.14103
-        y = torch.sigmoid(q) * torch.cumsum(torch.sigmoid(k) * v, dim=2)
+        # make head be batch dim, i.e. (B, T, nh, hs) -> (B, nh, T, hs)
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+        y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, is_causal=True)
         y = y.transpose(1, 2).contiguous().view(B, T, -1) # re-assemble all head outputs side by side
 
         # output projection
@@ -87,9 +86,8 @@ class Strawberry(nn.Module):
         self.config = config
 
         # factorized token embeddings
-        self.embed = nn.Sequential(nn.Embedding(config.vocab_size, config.n_embd))
+        self.embed = nn.Embedding(config.vocab_size, config.n_embd)
         self.blocks = nn.ModuleList([AttentionOnDetail(config) for _ in range(config.n_layer)])
-        self.unembed = CastedLinear(config.n_embd, config.vocab_size)
 
         # to support meta device initialization, we init the rotary embeddings here, but it's fake
         # as for rotary_seq_len, these rotary embeddings are pretty small/cheap in memory,
@@ -98,9 +96,6 @@ class Strawberry(nn.Module):
         self.rotary_block_size = config.block_size * 10 # 10X over-compute should be enough, TODO make nicer?
         d_head = config.n_qkv // config.n_head
         self.cos, self.sin = self._precompute_rotary_embeddings(self.rotary_block_size, d_head)
-
-        # zero out classifier weights
-        torch.nn.init.zeros_(self.unembed.w)
 
     def _precompute_rotary_embeddings(self, block_size, d_head, base=10000):
         # stride the channels
@@ -111,8 +106,7 @@ class Strawberry(nn.Module):
         # calculate the rotation frequencies at each (time, channel) pair
         freqs = torch.outer(t, inv_freq)
         cos, sin = freqs.cos(), freqs.sin()
-        cos, sin = cos[None, :, None, :], sin[None, :, None, :] # add batch and head dims for later broadcasting
-        return cos, sin
+        return cos[None, :, None, :], sin[None, :, None, :] # add batch and head dims for later broadcasting
 
     def forward(self, idx, targets=None):
         B, T = idx.size()
@@ -123,11 +117,18 @@ class Strawberry(nn.Module):
 
         x = self.embed(idx) # token embeddings of shape (b, t, n_embd)
         x = norm(x)
-        for _ in range(self.config.r_layer):
-            for block in self.blocks:
+
+        for block in self.blocks:
+            for _ in range(self.config.r_layer):
                 x = block(x, cos_sin)
+
         x = norm(x)
-        logits = self.unembed(x)
+        logits = F.linear(x, self.embed) # tying embed & unembed weights by using embed weights to unembed `x`
+
+        # forward the lm_head (compute logits)
+        softcap = 15 # smoothly cap the logits to the range [-softcap, softcap]
+        logits = softcap * torch.tanh(logits / softcap) # squash the logits
+
         # if we are given some desired targets also calculate the loss
         loss = None if targets is None else F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1, reduction="mean")
         return logits, loss
